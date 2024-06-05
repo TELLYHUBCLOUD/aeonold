94f90e99aded2698bb71f53090815aae37d0b24b
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-06-05 20:14:58 +0530

    Update update.py

--- update.py
+++ update.py
@@ -52,11 +52,11 @@ if DATABASE_URL:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://github.com/5hojib/Aeon'
+    UPSTREAM_REPO = 'https://github.com/TELLYHUBCLOUD/aeonold'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'main'
+    UPSTREAM_BRANCH = 'hub'
 
 if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
