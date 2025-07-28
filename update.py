68a2786a0058d2257065942df0fff0885eb0eae4
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2025-07-28 11:21:49 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,76 +1,142 @@
-from logging import FileHandler, StreamHandler, INFO, basicConfig, error as log_error, info as log_info
-from os import path as ospath, environ, execl as osexecl
+import os
+from datetime import datetime
+from importlib import import_module
+from logging import (
+    ERROR,
+    INFO,
+    FileHandler,
+    Formatter,
+    LogRecord,
+    StreamHandler,
+    basicConfig,
+    getLogger,
+)
+from logging import (
+    error as log_error,
+)
+from logging import (
+    info as log_info,
+)
+from os import path, remove
 from subprocess import run as srun
-from requests import get as rget
-from dotenv import load_dotenv
-from sys import executable
-from pymongo import MongoClient
+from sys import exit
 
-if ospath.exists('log.txt'):
-    with open('log.txt', 'r+') as f:
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from pytz import timezone
+
+getLogger("pymongo").setLevel(ERROR)
+
+if path.exists("log.txt"):
+    with open("log.txt", "r+") as f:
         f.truncate(0)
 
-basicConfig(format='%(levelname)s | From %(name)s -> %(module)s line no: %(lineno)d | %(message)s',
-                    handlers=[FileHandler('log.txt'), StreamHandler()], level=INFO)
+if path.exists("rlog.txt"):
+    remove("rlog.txt")
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
+
+class CustomFormatter(Formatter):
+    def formatTime(
+        self,
+        record: LogRecord,
+        datefmt: str | None,
+    ) -> str:
+        dt: datetime = datetime.fromtimestamp(
+            record.created,
+            tz=timezone("Asia/Dhaka"),
+        )
+        return dt.strftime(datefmt)
+
+    def format(self, record: LogRecord) -> str:
+        return super().format(record).replace(record.levelname, record.levelname[:1])
+
+
+formatter = CustomFormatter(
+    "[%(asctime)s] %(levelname)s - %(message)s [%(module)s:%(lineno)d]",
+    datefmt="%d-%b %I:%M:%S %p",
+)
+
+file_handler = FileHandler("log.txt")
+file_handler.setFormatter(formatter)
+
+stream_handler = StreamHandler()
+stream_handler.setFormatter(formatter)
+
+basicConfig(handlers=[file_handler, stream_handler], level=INFO)
+
 try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
+    settings = import_module("config")
+    config_file = {
+        key: value.strip() if isinstance(value, str) else value
+        for key, value in vars(settings).items()
+    }
+except Exception:
+    log_info(
+        "The 'config.py' file is missing! Falling back to environment variables.",
+    )
+    config_file = {}
 
-load_dotenv('config.env', override=True)
-
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-if len(BOT_TOKEN) == 0:
-    log_error("BOT_TOKEN variable is missing! Exiting now")
+BOT_TOKEN = config_file.get("BOT_TOKEN") or os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    log_error("BOT_TOKEN variable is missing! Exiting now.")
     exit(1)
 
-bot_id = BOT_TOKEN.split(':', 1)[0]
+BOT_ID = BOT_TOKEN.split(":", 1)[0]
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
-if len(DATABASE_URL) == 0:
-    DATABASE_URL = None
+DATABASE_URL = config_file.get("DATABASE_URL", "") or os.getenv("DATABASE_URL", "")
 
 if DATABASE_URL:
-    conn = MongoClient(DATABASE_URL)
-    db = conn.luna
-    if config_dict := db.settings.config.find_one({'_id': bot_id}):
-        environ['UPSTREAM_REPO'] = config_dict['UPSTREAM_REPO']
-        environ['UPSTREAM_BRANCH'] = config_dict['UPSTREAM_BRANCH']
-    conn.close()
+    try:
+        conn = MongoClient(DATABASE_URL, server_api=ServerApi("1"))
+        db = conn.luna
+        config_dict = db.settings.config.find_one({"_id": BOT_ID})
+        if config_dict is not None:
+            config_file["UPSTREAM_REPO"] = config_dict.get(
+                "UPSTREAM_REPO",
+                config_file.get("UPSTREAM_REPO"),
+            )
+            config_file["UPSTREAM_BRANCH"] = config_dict.get(
+                "UPSTREAM_BRANCH",
+                config_file.get("UPSTREAM_BRANCH"),
+            )
+        conn.close()
+    except Exception as e:
+        log_error(f"Database ERROR: {e}")
 
-UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
-if len(UPSTREAM_REPO) == 0:
-    UPSTREAM_REPO = 'https://github.com/TELLYHUBCLOUD/aeonold'
+UPSTREAM_REPO = (
+    config_file.get("UPSTREAM_REPO", "")
+    or os.getenv("UPSTREAM_REPO", "")
+    or "https://github.com/TELLYHUBCLOUD/aeonold"
+)
 
-UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
-if len(UPSTREAM_BRANCH) == 0:
-    UPSTREAM_BRANCH = 'hub'
+UPSTREAM_BRANCH = (
+    config_file.get("UPSTREAM_BRANCH", "")
+    or os.getenv("UPSTREAM_BRANCH", "")
+    or "hub"
+)
 
-if ospath.exists('.git'):
-    srun(["rm", "-rf", ".git"])
+if UPSTREAM_REPO:
+    if path.exists(".git"):
+        srun(["rm", "-rf", ".git"], check=False)
 
-update = srun([f"git init -q \
-                 && git config --global user.email yesiamshojib@gmail.com \
-                 && git config --global user.name 5hojib \
-                 && git add . \
-                 && git commit -sm update -q \
-                 && git remote add origin {UPSTREAM_REPO} \
-                 && git fetch origin -q \
-                 && git reset --hard origin/{UPSTREAM_BRANCH} -q"], shell=True)
+    update = srun(
+        [
+            f"git init -q \
+                     && git config --global user.email e.anastayyar@gmail.com \
+                     && git config --global user.name mltb \
+                     && git add . \
+                     && git commit -sm update -q \
+                     && git remote add origin {UPSTREAM_REPO} \
+                     && git fetch origin -q \
+                     && git reset --hard origin/{UPSTREAM_BRANCH} -q",
+        ],
+        shell=True,
+        check=False,
+    )
 
-if update.returncode == 0:
-    log_info('Successfully updated with latest commit from UPSTREAM_REPO')
-else:
-    log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+    if update.returncode == 0:
+        log_info("Successfully updated with latest commit from UPSTREAM_REPO")
+    else:
+        log_error(
+            "Something went wrong while updating, check UPSTREAM_REPO if valid or not!",
+        )
